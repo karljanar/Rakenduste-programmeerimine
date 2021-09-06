7f3bcbb779493bc129a85d2e26c3ec430c42464c
karljanar <karljk@tlu.ee>
2021-09-06 10:59:10 +0300

    first commit

---
+++ 2n/react-frontend/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Item from './components/Item'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         hello
+        </p>
+        <Item />
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
